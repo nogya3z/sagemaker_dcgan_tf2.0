ee8809533d2122dfc0b4426120aa9e2a8953464e
noahgreher <n0gt27@aol.com>
2020-01-03 00:20:21 -0500

    horovod code works but doesn't distribute

--- container/dcgan.py
+++ container/dcgan.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 
 import argparse
 import functools
+import horovod.tensorflow as hvd
+import math
 import numpy as np
 import os
 import tensorflow as tf
@@ -14,7 +16,17 @@ from PIL import Image
 from read_tfrecords import read
 from tensorflow.keras import layers
 
-batch_size = 256
+BATCH_SIZE = 256
+
+hvd.init()
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+if gpus:
+    tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+
 
 
 #https://stackoverflow.com/questions/30227466/combine-several-images-horizontally-with-python
@@ -109,8 +121,8 @@ def discriminator_loss(real_output, fake_output):
 def generator_loss(fake_output):
     return cross_entropy(tf.ones_like(fake_output), fake_output)
 
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+generator_optimizer = tf.keras.optimizers.Adam(1e-4* hvd.size())
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4* hvd.size())
 
 #checkpoint_dir = model_dir
 #checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
@@ -128,23 +140,34 @@ num_examples_to_generate = 16
 seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
 @tf.function
-def train_step(images):
-    noise = tf.random.normal([batch_size, noise_dim])
+def train_step(images, epoch):
+  noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-      generated_images = generator(noise, training=True)
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    generated_images = generator(noise, training=True)
 
-      real_output = discriminator(images, training=True)
-      fake_output = discriminator(generated_images, training=True)
+    real_output = discriminator(images, training=True)
+    fake_output = discriminator(generated_images, training=True)
 
-      gen_loss = generator_loss(fake_output)
-      disc_loss = discriminator_loss(real_output, fake_output)
+    gen_loss = generator_loss(fake_output)
+    disc_loss = discriminator_loss(real_output, fake_output)
 
-    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+  gen_tape = hvd.DistributedGradientTape(gen_tape)
+  disc_tape = hvd.DistributedGradientTape(disc_tape)
 
-    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+  gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+  gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+
+  generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+  if epoch == 0:
+    hvd.broadcast_variables(generator.variables, root_rank=0)
+    hvd.broadcast_variables(discriminator.variables, root_rank=0)
+    hvd.broadcast_variables(generator_optimizer.variables(), root_rank=0)
+    hvd.broadcast_variables(discriminator_optimizer.variables(), root_rank=0)
+
+
     
 def generate_and_save_images(model, epoch, test_input, model_dir):
   img_folder = model_dir+"/images"
@@ -161,12 +184,8 @@ def generate_and_save_images(model, epoch, test_input, model_dir):
 
     
 def train(data_dir, epochs, model_dir):
-  print("hellow world")
   print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
   print("Name of GPU's: ", (tf.config.experimental.list_physical_devices('GPU')))
-  print("is a gpu available?")
-  print(tf.test.is_gpu_available(cuda_only=False,min_cuda_compute_capability=None))
-
 
 
   print(os.listdir(data_dir))
@@ -184,7 +203,7 @@ def train(data_dir, epochs, model_dir):
     start = time.time()
 
     for image_batch in dataset:
-      train_step(image_batch)
+      train_step(image_batch, epoch)
 
     # Produce images for the GIF as we go
     generate_and_save_images(generator,
@@ -192,7 +211,7 @@ def train(data_dir, epochs, model_dir):
                              seed, model_dir)
 
     # Save the model every 15 epochs
-    if (epoch + 1) % 15 == 0:
+    if (epoch + 1) % 15 == 0 and  hvd.rank() == 0:
       checkpoint.save(file_prefix = checkpoint_prefix)
 
     print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
@@ -204,8 +223,9 @@ def train(data_dir, epochs, model_dir):
 
 
 def main(data_dir, epochs, model_dir):
-    with tf.device('/GPU:0'):
-        train(data_dir, epochs, model_dir)
+    epochs = int(math.ceil(epochs / hvd.size()))
+    #adjust the epochs for distributed training.
+    train(data_dir, epochs, model_dir)
 
 
 if __name__ == '__main__':
